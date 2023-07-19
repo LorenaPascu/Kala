14ab3aa64a75c7ffa112cde9064b730d417f2498
LorenaPascu <pasculorenaclaudia@yahoo.com>
2023-07-19 21:25:40 +0300

    Add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,55 @@
 function App() {
   return (
     <>
+    <nav>
+      <div>
+        <a href="">Home</a>
+        <a href="">User</a>
+        <a href=""><img src=""/></a>
+      </div>
+
+       <article>
+        <h3>Your Playlists</h3>
+
+        <div>
+          <h4>Playlist1</h4>
+        </div>
+         
+        <div>
+          <h4>Playlist2</h4>
+        </div> 
+       </article>
+    </nav>
+
+    <header>
+      <div>
+        <h1 className="text-3xl font-bold underline">Kala Music</h1>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </div>
+
+      <div>
+        <img src=""/>
+      </div>
+    </header>
+
+    <section>
+     <article>
+      <h2>Recently played</h2>
+     </article>
+
+     <article>
+      <h2>Mixes for you</h2>
+     </article>
+
+     <article>
+      <h2>Recomanded</h2>
+     </article>
+
+     <article>
+      <h2>Random pick</h2>
+     </article>
+    </section>
+
     </>
   )
 }
